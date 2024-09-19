763277dab62bbd3633ac9984d30405695c6429ab
SheilaS3 <sheilaportu@hotmail.com>
2024-09-20 00:58:44 +0200

    Used axios to perform get request to fetch recents posts and setup the required reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
+import { SET_RECENT_POSTS } from './types';
+
+import axios from 'axios';
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        //perform our request in here
-        console.log("Hello");
+        axios
+        .get('https://pokeapi.co/api/v2/pokemon')
+        .then(response => {
+            console.log(response.data.results);
+            dispatch({
+                type: SET_RECENT_POSTS,
+                payload: response.data.results
+            })
+        })
     }
 }
