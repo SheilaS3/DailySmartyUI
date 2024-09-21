623483c794cb09c0e971bd603816a7b7da2b04a4
SheilaS3 <sheilaportu@hotmail.com>
2024-09-21 18:02:08 +0200

    Added post topics to post component

--- src/components/post.js
+++ src/components/post.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class Post extends Component {
     render() {
         return (
-            <li>
-                {this.props.name}
+            <li className="recent-post">
+                <div className="recent-post__title">
+                    {this.props.name}
+                </div>
+
+                <div className="recent-post__topics">
+                    {this.props.url}
+                </div>
             </li>
         )
     }
